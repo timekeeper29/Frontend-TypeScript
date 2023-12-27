c4c24b25899034a83cff1574c17cf7d299559435
timekeeper29 <nagatadaichi36@gmail.com>
2023-12-27 12:55:45 +0200

    initial implementation of navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom"
 import Home from './pages/home/home';
 import Login from './pages/login/login';
 import Signup from './pages/signup/signup';
+import Navbar from './pages/navbar/navbar';
 
 
 function App() {
   return (
-
     <>
-      <h1>ofek</h1>
+			<Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
